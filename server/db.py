9f505597719f509eefff2d92808d675357df1e9d
Beknur <Beknurarystan@gmail.com>
2025-07-25 06:41:26 +0500

    последний commit деплои бэкенд связь БД

--- server/db.py
+++ server/db.py
@@ -1,11 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from server.config import DATABASE_URL
-from server.models import Base
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-# Создание таблиц (если нужно)
-Base.metadata.create_all(bind=engine)
-
